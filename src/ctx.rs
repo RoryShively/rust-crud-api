49efcb29205180b56c2b80ffe44fba1617bffd33
RoryShively <roryshively1@gmail.com>
2023-05-27 20:47:05 -0500

    added ctx

---
+++ src/ctx.rs
@@ -0,0 +1,11 @@
+#[derive(Clone, Debug)]
+pub struct Ctx {
+    user_id: u64,
+}
+
+// Constructor.
+impl Ctx {
+    pub fn new(user_id: u64) -> Self {
+        Self { user_id }
+    }
+}
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,5 +1,10 @@
+use crate::ctx::Ctx;
 use crate::{Error, Result};
 
+use async_trait::async_trait;
+use axum::RequestPartsExt;
+use axum::extract::FromRequestParts;
+use axum::http::request::Parts;
 use axum::response::Response;
 use axum::middleware::Next;
 use axum::http::Request;
@@ -8,24 +13,47 @@ use tower_cookies::Cookies;
 
 use crate::web::AUTH_TOKEN;
 
+// Look @ 55:30 https://www.youtube.com/watch?v=XZtlD_m59sM
+// which explains injecting Ctx, I dont fully understand atm
+
 pub async fn mw_require_auth<B>(
-    cookies: Cookies,
+    ctx: Result<Ctx>,
     req: Request<B>, 
     next: Next<B>
 ) -> Result<Response> {
-    println!("->> {:<12} - mw_require_auth", "MIDDLEWARE");
+    println!("->> {:<12} - mw_require_auth - {ctx:?}", "MIDDLEWARE");
 
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
-
-    // Parse Token
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
-
-    // TODO: Token components validation
+    ctx?;
 
     Ok(next.run(req).await)
 }
+
+// region:     --- Ctx Extractor
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - Ctx", "EXTRACTOR");
+
+        // User the cookies extractor
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+        // Parse token
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        // TODO: Token components validation
+
+        Ok(Ctx::new(user_id))
+    }
+}
+
+// endregion:  --- Ctx Extractor
+
 
 /// Parse a token of format `user-[user-id].[expiration].[signature]`
 /// Returns (user_id, expiration, signature)
