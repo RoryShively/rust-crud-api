0a52377cc97777cf5fbcd8794d22576a756aa259
RoryShively <roryshively1@gmail.com>
2023-05-28 02:09:22 -0500

    added structured logging

--- src/error.rs
+++ src/error.rs
@@ -6,10 +6,12 @@ https://docs.rs/thiserror/latest/thiserror/
  */
 
 use axum::{response::{IntoResponse, Response}, http::StatusCode};
+use serde::Serialize;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, Clone, strum_macros::AsRefStr)]
+#[derive(Debug, Clone, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
 pub enum Error {
     LoginFail,
 
